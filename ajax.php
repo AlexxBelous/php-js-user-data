6acde503d3e5c15286c43819048c1626689854cd
AlexxBelous <connectionwithme1@gmail.com>
2025-01-05 19:01:04 +0200

    finished for__part__1

--- ajax.php
+++ ajax.php
@@ -1,4 +1,29 @@
 <?php
 
+$type = $_POST['type'];
+if ($type == 'yesno') {
+    yesNoLog();
+}
+
+function yesNoLog()
+{
+    $articleid = $_POST['articleid'];
+    $result = $_POST['result'];
+    try {
+        $host = 'mysql';
+        $dbname = 'bookexamples';
+        $user = 'root';
+        $pass = 'root';
+        $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $sql = "insert into `yesno` (`articleid`, `result`) values (?, ?);";
+        $sth = $DBH->prepare($sql);
+        $sth->bindParam(1, $articleid, PDO::PARAM_INT);
+        $sth->bindParam(2, $result, PDO::PARAM_STR);
+        $sth->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
 
 ?>
--- correct-answer/answer.php
+++ correct-answer/answer.php
@@ -33,33 +33,18 @@ function yesNoLog()
 
 
 
-<!--Script JQUERY-->
+
+<!------------------------Main.js------------------------>
 
 <script>
-    // Устанавливаем обработчик события click для всех радио-кнопок с именем "answer"
-    $("input:radio[name=answer]").click(function () {
-        // Получаем значение выбранной радио-кнопки
-        var val = $(this).val();
-        // Передаем значение выбранного ответа в функцию saveAnswer
-        saveAnswer(val);
-    });
+    import {saveAnswer} from "./saveAnswer.js";
 
-    // Функция для сохранения ответа
-    function saveAnswer(answer) {
-        // Отправляем POST-запрос на сервер
-        $.post(
-            "ajax.php",            // URL для отправки запроса
-            {                      // Данные, отправляемые на сервер
-                type: "yesno",     // Тип ответа (например, да/нет)
-                articleid: "1221", // Идентификатор статьи
-                result: answer     // Ответ пользователя
-            }
-        )
-            .done(function (data) {    // Выполняется, если запрос успешно обработан
-                // Показываем сообщение об успешной отправке данных
-                alert('Thank you!' + data);
-            });
-    }
+    document.querySelectorAll("input[type=radio][name=answer]").forEach(function (radio) {
+        radio.addEventListener("click", function () {
+            const val = this.value;
+            saveAnswer(val)
+        });
+    });
 
 </script>
 
@@ -67,45 +52,16 @@ function yesNoLog()
 
 
 
-
-
-<!--Script JS-->
+<!------------------------saveAnswer.js------------------------>
 
 <script>
-    // Находим все радио-кнопки с типом input[type=radio] и именем answer
+    import {saveAnswer} from "./saveAnswer.js";
+
     document.querySelectorAll("input[type=radio][name=answer]").forEach(function (radio) {
-        // Добавляем обработчик события click для каждой найденной радио-кнопки
         radio.addEventListener("click", function () {
-            // Получаем значение выбранной радио-кнопки
             const val = this.value;
-            // Передаем значение в функцию saveAnswer для сохранения
-            saveAnswer(val);
+            saveAnswer(val)
         });
     });
-
-    // Функция для сохранения ответа
-    function saveAnswer(answer) {
-        // Создаем объект FormData для отправки данных
-        const data = new FormData();
-        data.append("type", "yesno");       // Тип ответа (например, да/нет)
-        data.append("articleid", "1221");  // Идентификатор статьи
-        data.append("result", answer);     // Значение ответа пользователя
-
-        // Отправляем запрос на сервер с помощью fetch
-        fetch("ajax.php", {
-            method: "POST", // Метод запроса - POST
-            body: data      // Тело запроса содержит объект FormData
-        })
-            .then(response => response.text()) // Преобразуем ответ сервера в текст
-            .then(data => {
-                // Если запрос успешен, показываем сообщение с данными от сервера
-                alert('Thank you! ' + data);
-            })
-            .catch(error => {
-                // В случае ошибки выводим сообщение в консоль
-                console.error('Error:', error);
-            });
-    }
-
 
 </script>
