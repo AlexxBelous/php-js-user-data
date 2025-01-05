34df014c55ae4aec9b895c8518934a486b49a303
AlexxBelous <connectionwithme1@gmail.com>
2025-01-05 19:58:41 +0200

    for part 2

--- sql/create.sql
+++ sql/create.sql
@@ -6,3 +6,13 @@ CREATE TABLE `yesno`
     `result`    VARCHAR(10),
     PRIMARY KEY (`id`)
 )
+
+-- for__part__2-------------------------------------------
+CREATE TABLE `whyanswerlog`
+(
+    `id`        INT       NOT NULL AUTO_INCREMENT,
+    `mytime`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `articleid` INT,
+    `result`    VARCHAR(500),
+    PRIMARY KEY (`id`)
+);
