068dcb83f21a0cf2cded599ce1ef869122c18762
AlexxBelous <connectionwithme1@gmail.com>
2025-01-05 19:01:04 +0200

    finished for__part__1

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+import {saveAnswer} from "./saveAnswer.js";
+
+document.querySelectorAll("input[type=radio][name=answer]").forEach(function (radio) {
+    radio.addEventListener("click", function () {
+        const val = this.value;
+        saveAnswer(val)
+    });
+});
+
+
