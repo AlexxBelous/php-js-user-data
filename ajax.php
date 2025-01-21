c4c9d64864935f3280215f9b18b6d2a42266a889
Alex <connectionwithme1@gmail.com>
2025-01-21 14:23:53 +0200

    for__part__2-finished

--- ajax.php
+++ ajax.php
@@ -3,6 +3,8 @@
 $type = $_POST['type'];
 if ($type == 'yesno') {
     yesNoLog();
+} else if ($type == 'whyanswer') {
+    whyAnswerLog();
 }
 
 function yesNoLog()
@@ -18,6 +20,28 @@ function yesNoLog()
         $sql = "insert into `yesno` (`articleid`, `result`) values (?, ?);";
         $sth = $DBH->prepare($sql);
         $sth->bindParam(1, $articleid, PDO::PARAM_INT);
+        $sth->bindParam(2, $result, PDO::PARAM_STR);
+        $sth->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+}
+
+function whyAnswerLog()
+{
+    $articlesid = $_POST['articleid'];
+    $result = $_POST['result'];
+
+    try {
+        $host = 'mysql';
+        $dbname = 'bookexamples';
+        $user = 'root';
+        $pass = 'root';
+        $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $sql = "insert into `whyanswerlog` (`articleid`, `result`) values (?, ?);";
+        $sth = $DBH->prepare($sql);
+        $sth->bindParam(1, $articlesid, PDO::PARAM_INT);
         $sth->bindParam(2, $result, PDO::PARAM_STR);
         $sth->execute();
     } catch (PDOException $e) {
--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,22 @@
 import {saveAnswer} from "./saveAnswer.js";
+import {saveWhyAnswer} from "./saveWhyAnswer.js";
 
 document.querySelectorAll("input[type=radio][name=answer]").forEach(function (radio) {
     radio.addEventListener("click", function () {
         const val = this.value;
         saveAnswer(val)
+
+        if (val === 'No') {
+            showForm();
+        }
     });
 });
+
+
+function showForm() {
+    document.getElementById("explainwhy").style.display = "block";
+}
+
+const btnFormAnswer = document.getElementById("btnFormAnswer");
+btnFormAnswer.addEventListener("click", saveWhyAnswer);
+
