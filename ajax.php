fafb11ecb8b45c378c8d3cb428b6bcb58df22445
Alex <connectionwithme1@gmail.com>
2025-01-21 14:54:52 +0200

    for__part__3-finished

--- ajax.php
+++ ajax.php
@@ -1,37 +1,79 @@
 <?php
 
-$type = $_POST['type'];
-if ($type == 'yesno') {
-    yesNoLog();
-} else if ($type == 'whyanswer') {
-    whyAnswerLog();
+
+$contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+
+
+
+if (str_contains($contentType, 'application/json')) {
+    $data = json_decode(file_get_contents('php://input'), true);
+} else {
+
+    $data = $_POST;
 }
 
-function yesNoLog()
+
+if (!isset($data['type'])) {
+    echo json_encode(['status' => 'error', 'message' => 'Type is not defined']);
+    die();
+}
+
+
+switch ($data['type']) {
+    case 'yesno':
+        yesNoLog($data);
+        break;
+    case 'whyanswer':
+        whyAnswer($data);
+        break;
+    case 'starrating':
+        starRatingLog($data);
+        break;
+    default:
+        echo json_encode(['status' => 'error', 'message' => 'Unknown type']);
+        break;
+}
+
+
+function yesNoLog($data)
 {
-    $articleid = $_POST['articleid'];
-    $result = $_POST['result'];
+    $articleid = $data['articleid'] ?? null;
+    $result = $data['result'] ?? null;
+
+    if (!$articleid || !$result) {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid yesno data']);
+        return;
+    }
+
     try {
         $host = 'mysql';
         $dbname = 'bookexamples';
         $user = 'root';
         $pass = 'root';
         $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-        $sql = "insert into `yesno` (`articleid`, `result`) values (?, ?);";
+
+        $sql = "INSERT INTO `yesno` (`articleid`, `result`) VALUES (?, ?)";
         $sth = $DBH->prepare($sql);
         $sth->bindParam(1, $articleid, PDO::PARAM_INT);
         $sth->bindParam(2, $result, PDO::PARAM_STR);
         $sth->execute();
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
 
+        echo json_encode(['status' => 'success', 'message' => 'Yes or No saved']);
+    } catch (PDOException $e) {
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    }
 }
 
-function whyAnswerLog()
+
+function whyAnswer($data)
 {
-    $articlesid = $_POST['articleid'];
-    $result = $_POST['result'];
+    $articleid = $data['articleid'] ?? null;
+    $result = $data['result'] ?? null;
+
+    if (!$articleid || !$result) {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid whyanswer data']);
+        return;
+    }
 
     try {
         $host = 'mysql';
@@ -39,15 +81,51 @@ function whyAnswerLog()
         $user = 'root';
         $pass = 'root';
         $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-        $sql = "insert into `whyanswerlog` (`articleid`, `result`) values (?, ?);";
+
+        $sql = "INSERT INTO `whyanswerlog` (`articleid`, `result`) VALUES (?, ?)";
         $sth = $DBH->prepare($sql);
-        $sth->bindParam(1, $articlesid, PDO::PARAM_INT);
+        $sth->bindParam(1, $articleid, PDO::PARAM_INT);
         $sth->bindParam(2, $result, PDO::PARAM_STR);
         $sth->execute();
+
+        echo json_encode(['status' => 'success', 'message' => 'Why answer saved']);
     } catch (PDOException $e) {
-        echo $e->getMessage();
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
     }
 }
 
 
-?>
+function starRatingLog($data)
+{
+    $articleid = $data['articleid'] ?? null;
+    $result = $data['userrating'] ?? null;
+
+
+    if (!$articleid || !is_numeric($result)) {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid starrating data']);
+        return;
+    }
+
+    try {
+        $host = 'mysql';
+        $dbname = 'bookexamples';
+        $user = 'root';
+        $pass = 'root';
+        $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+
+        $sql = "INSERT INTO `starratinglog` (`articleid`, `result`) VALUES (?, ?)";
+        $sth = $DBH->prepare($sql);
+
+        // Привязка параметров
+        $sth->bindParam(1, $articleid, PDO::PARAM_INT);  // articleid остается целым числом
+        $sth->bindValue(2, $result); // передаем float напрямую, без указания типа
+
+        $sth->execute();
+
+        echo json_encode(['status' => 'success', 'message' => 'Star rating saved']);
+    } catch (PDOException $e) {
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+}
+
--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,17 @@
 import {saveAnswer} from "./saveAnswer.js";
 import {saveWhyAnswer} from "./saveWhyAnswer.js";
+import {initRateYo} from "./saveRatings.js";
 
 document.querySelectorAll("input[type=radio][name=answer]").forEach(function (radio) {
     radio.addEventListener("click", function () {
         const val = this.value;
-        saveAnswer(val)
+        saveAnswer(val);
 
         if (val === 'No') {
             showForm();
+        }
+        if (val === 'Yes') {
+            showStars();
         }
     });
 });
@@ -19,4 +23,14 @@ function showForm() {
 
 const btnFormAnswer = document.getElementById("btnFormAnswer");
 btnFormAnswer.addEventListener("click", saveWhyAnswer);
+
+
+function showStars() {
+    const ratingsContainer = document.getElementById("ratingsContainer");
+    ratingsContainer.style.display = "block";
+    initRateYo();
+}
+
+
+
 
